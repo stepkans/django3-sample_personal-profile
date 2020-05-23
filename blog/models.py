f344fe6ddc0bca8587edfaab6d6b20b9ad956619
Stephen Suyanka <stepkans@gmail.com>
2020-05-23 10:53:01 +0300

    First Commit!

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=False)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
